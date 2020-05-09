f4104b718b27bdeb18bc4aa59c9774582371e535
phil.jambaud@gmail.com <&[GzLj2?@qP9"uVD>
2020-05-09 16:18:54 +0200

    Add WinEvent

---
+++ src/main/java/io/github/ph1lou/pluginlgapi/WinEvent.java
@@ -0,0 +1,37 @@
+package io.github.ph1lou.pluginlgapi;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+import java.util.List;
+import java.util.UUID;
+
+public class WinEvent extends Event {
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+    private final String role;
+    private final List<UUID> players;
+
+
+    public WinEvent(String role, List<UUID> players) {
+        this.role = role;
+        this.players = players;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS_LIST;
+    }
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public List<UUID> getPlayers() {
+        return players;
+    }
+}
