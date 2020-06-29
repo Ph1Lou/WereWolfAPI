6f6bb9d0b39543d078dae0fd21b9993f380e458b
phil.jambaud@gmail.com <&[GzLj2?@qP9"uVD>
2020-06-29 14:04:49 +0200

    - add Score methods

--- src/main/java/io/github/ph1lou/werewolfapi/WereWolfAPI.java
+++ src/main/java/io/github/ph1lou/werewolfapi/WereWolfAPI.java
@@ -259,23 +259,6 @@ public interface WereWolfAPI {
 
     void death(UUID uuid);
 
-    /**
-     * return the time rearranged HH:MM:SS
-     * @param timer time in second
-     * @return String
-     */
-
-    String conversion(int timer);
-
-    /**
-     * return the arrow to reach the target location
-     * @param player current player
-     * @param target target location
-     * @return String
-     */
-
-    String updateArrow(Player player, Location target);
-
     Map<String,String> getLanguage();
 
     Random getRandom();
@@ -292,12 +275,8 @@ public interface WereWolfAPI {
 
     void updateNameTag();
 
-    void setRoleSize(int roleSize);
-
-    int getRoleSize();
-
     StuffManager getStuffs();
 
-    int getPlayerSize();
+    ScoreAPI getScore();
 
 }
