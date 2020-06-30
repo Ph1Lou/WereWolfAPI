37421810834ac8180fde166f62856db9be0c079f
phil.jambaud@gmail.com <&[GzLj2?@qP9"uVD>
2020-06-30 17:01:06 +0200

    - add Pvp event

---
+++ src/main/java/io/github/ph1lou/werewolfapi/events/PVPEvent.java
@@ -0,0 +1,20 @@
+package io.github.ph1lou.werewolfapi.events;
+
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class PVPEvent extends Event {
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+
+
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS_LIST;
+    }
+
+}
