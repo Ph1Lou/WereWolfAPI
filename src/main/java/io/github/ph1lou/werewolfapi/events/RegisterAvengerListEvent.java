b96f09e624481a7454709d9704a2874ae5b08da4
Ph1Lou <&[GzLj2?@qP9"uVD>
2021-03-28 12:20:21 +0200

    - add avenger werewolf

---
+++ src/main/java/io/github/ph1lou/werewolfapi/events/RegisterAvengerListEvent.java
@@ -0,0 +1,26 @@
+package io.github.ph1lou.werewolfapi.events;
+
+import io.github.ph1lou.werewolfapi.PlayerWW;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class RegisterAvengerListEvent extends SelectionEvent {
+
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+
+    public RegisterAvengerListEvent(PlayerWW playerWW, PlayerWW targetWW, boolean isWereWolf) {
+        super(playerWW,targetWW);
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS_LIST;
+    }
+
+}
