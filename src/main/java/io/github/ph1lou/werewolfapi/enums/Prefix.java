52df36200f63197cdd4b73ba7494dd05dd6c7ac6
Ph1Lou <email@email.com>
2021-11-14 13:20:35 +0100

    update text

---
+++ src/main/java/io/github/ph1lou/werewolfapi/enums/Prefix.java
@@ -0,0 +1,19 @@
+package io.github.ph1lou.werewolfapi.enums;
+
+public enum Prefix {
+    YELLOW("werewolf.prefix.yellow"),
+    RED("werewolf.prefix.red"),
+    GREEN("werewolf.prefix.green"),
+    ORANGE("werewolf.prefix.orange"),
+    BLUE("werewolf.prefix.blue"),
+    LIGHT_BLUE("werewolf.prefix.light_blue");
+
+    private final String key;
+    Prefix(String key){
+        this.key=key;
+    }
+
+    public String getKey() {
+        return key;
+    }
+}
