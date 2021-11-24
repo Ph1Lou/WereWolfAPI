2f0c2a27b1954b181a856d0929d9c24aa1995086
Ph1Lou <email@email.com>
2021-11-24 15:22:58 +0100

    fix potion

--- src/main/java/io/github/ph1lou/werewolfapi/versions/VersionUtils.java
+++ src/main/java/io/github/ph1lou/werewolfapi/versions/VersionUtils.java
@@ -1,8 +1,5 @@
 package io.github.ph1lou.werewolfapi.versions;
 
-
-import com.pieterdebot.biomemapping.Biome;
-import com.pieterdebot.biomemapping.BiomeMappingAPI;
 import io.github.ph1lou.werewolfapi.enums.UniversalMaterial;
 import org.bukkit.Location;
 import org.bukkit.OfflinePlayer;
@@ -56,48 +53,6 @@ public abstract class VersionUtils {
     public abstract void removePlayerMaxHealth(@NotNull Player player, double health);
 
     public abstract double getPlayerMaxHealth(@NotNull Player player);
-
-    public void patchBiomes(){
-        BiomeMappingAPI biome = new BiomeMappingAPI();
-        replaceBiome(biome, Biome.BEACH, Biome.FOREST);
-        replaceBiome(biome, Biome.WOODED_HILLS, Biome.PLAINS);
-        replaceBiome(biome, Biome.BAMBOO_JUNGLE, Biome.FOREST);
-        replaceBiome(biome, Biome.BAMBOO_JUNGLE_HILLS, Biome.FOREST);
-        replaceBiome(biome, Biome.JUNGLE, Biome.FOREST);
-        replaceBiome(biome, Biome.JUNGLE_HILLS, Biome.FOREST);
-        replaceBiome(biome, Biome.JUNGLE_EDGE, Biome.FOREST);
-        replaceBiome(biome, Biome.MODIFIED_JUNGLE, Biome.FOREST);
-        replaceBiome(biome, Biome.MODIFIED_JUNGLE_EDGE, Biome.FOREST);
-        replaceBiome(biome, Biome.TAIGA_MOUNTAINS, Biome.FOREST);
-        replaceBiome(biome, Biome.TAIGA_HILLS, Biome.FOREST);
-        replaceBiome(biome, Biome.SNOWY_TAIGA_MOUNTAINS, Biome.FOREST);
-        replaceBiome(biome, Biome.SNOWY_TAIGA_HILLS, Biome.FOREST);
-        replaceBiome(biome, Biome.GRAVELLY_MOUNTAINS, Biome.FOREST);
-        replaceBiome(biome, Biome.WOODED_MOUNTAINS, Biome.FOREST);
-        replaceBiome(biome, Biome.DESERT, Biome.FOREST);
-        replaceBiome(biome, Biome.DESERT_HILLS, Biome.FOREST);
-        replaceBiome(biome, Biome.DESERT_LAKES, Biome.FOREST);
-        replaceBiome(biome, Biome.COLD_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.WARM_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.LUKEWARM_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.FROZEN_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.DEEP_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.DEEP_WARM_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.DEEP_LUKEWARM_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.DEEP_FROZEN_OCEAN, Biome.FOREST);
-        replaceBiome(biome, Biome.DEEP_COLD_OCEAN, Biome.FOREST);
-    }
-
-    private void replaceBiome(BiomeMappingAPI biome, Biome initial, Biome finalBiome){
-        try {
-            if(biome.biomeSupported(initial)){
-                biome.replaceBiomes(initial, finalBiome);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
 
     public abstract <T> void setGameRuleValue(World world, String gameRule, T value);
 
