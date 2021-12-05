b8951536a2c532aa11c1b18f1e67bba6509e432b
Ph1Lou <email@email.com>
2021-12-05 14:43:44 +0100

    update api

--- src/main/java/io/github/ph1lou/werewolfapi/versions/VersionUtils_1_9.java
+++ src/main/java/io/github/ph1lou/werewolfapi/versions/VersionUtils_1_9.java
@@ -128,7 +128,5 @@ public class VersionUtils_1_9 extends VersionUtils_1_8 {
     public ItemStack getItemInHand(@NotNull Player player) {
         return player.getInventory().getItemInMainHand();
     }
-
-
 }
 
