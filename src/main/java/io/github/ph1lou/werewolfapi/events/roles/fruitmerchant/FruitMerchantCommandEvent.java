a057ea4e60569c8c65a7c849f9f899e5ed3783c3
phil <email@email.com>
2022-01-07 18:23:18 +0100

    add fruit merchant

---
+++ src/main/java/io/github/ph1lou/werewolfapi/events/roles/fruitmerchant/FruitMerchantCommandEvent.java
@@ -0,0 +1,52 @@
+package io.github.ph1lou.werewolfapi.events.roles.fruitmerchant;
+
+import io.github.ph1lou.werewolfapi.IPlayerWW;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Set;
+
+public class FruitMerchantCommandEvent extends Event implements Cancellable {
+
+    private final IPlayerWW playerWW;
+    private final Set<IPlayerWW> playerWWS;
+    private boolean cancel=false;
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+
+    public FruitMerchantCommandEvent(IPlayerWW playerWW, Set<IPlayerWW> playerWWS){
+        this.playerWW = playerWW;
+        this.playerWWS = playerWWS;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS_LIST;
+    }
+
+    public IPlayerWW getPlayerWW() {
+        return this.playerWW;
+    }
+
+    public Set<IPlayerWW> getPlayerWWS() {
+        return playerWWS;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel=cancel;
+    }
+}
+
---
+++ src/main/java/io/github/ph1lou/werewolfapi/events/roles/fruitmerchant/FruitMerchantRecoverInformationEvent.java
@@ -0,0 +1,60 @@
+package io.github.ph1lou.werewolfapi.events.roles.fruitmerchant;
+
+import io.github.ph1lou.werewolfapi.IPlayerWW;
+import org.bukkit.event.Cancellable;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Map;
+import java.util.Set;
+
+public class FruitMerchantRecoverInformationEvent extends Event implements Cancellable {
+
+    private final IPlayerWW playerWW;
+    private final Set<IPlayerWW> playerWWS;
+    private final Map<IPlayerWW,GoldenCount> goldenAppleCount;
+    private boolean cancel=false;
+
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+
+    public FruitMerchantRecoverInformationEvent(IPlayerWW playerWW, Set<IPlayerWW> playerWWS,
+                                                Map<IPlayerWW,GoldenCount> goldenAppleCount){
+        this.playerWW = playerWW;
+        this.playerWWS = playerWWS;
+        this.goldenAppleCount = goldenAppleCount;
+    }
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS_LIST;
+    }
+
+    public IPlayerWW getPlayerWW() {
+        return this.playerWW;
+    }
+
+    public Set<IPlayerWW> getPlayerWWS() {
+        return playerWWS;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return cancel;
+    }
+
+    @Override
+    public void setCancelled(boolean cancel) {
+        this.cancel=cancel;
+    }
+
+    public GoldenCount getGoldenAppleCount(IPlayerWW playerWW) {
+        return goldenAppleCount.get(playerWW);
+    }
+}
+
