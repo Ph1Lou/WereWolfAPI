af73c22b86022cf2923be39736a45e8041e72dc6
phil <email@email.com>
2022-01-15 11:18:08 +0100

    fix bug aura description builder

--- src/main/java/fr/ph1lou/werewolfapi/role/utils/DescriptionBuilder.java
+++ src/main/java/fr/ph1lou/werewolfapi/role/utils/DescriptionBuilder.java
@@ -82,7 +82,8 @@ public class DescriptionBuilder {
                                 : "werewolf.description.no")))
                 : "");
 
-        sb.append(this.game.translate("werewolf.description.aura", Formatter.format("&aur&", game.translate(this.role.getAura().getKey()))));
+        sb.append(this.game.translate("werewolf.description.aura", Formatter.format("&aura&",
+                game.translate(this.role.getAura().getKey()))));
 
         if(!this.role.isAbilityEnabled()){
             sb.append(game.translate("werewolf.description.disable"));
