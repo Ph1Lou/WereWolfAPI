848ea271cfd2d92444cc0b0b8595986dfd8c19a9
phil <email@email.com>
2022-05-16 23:13:17 +0200

    update api

--- src/main/java/fr/ph1lou/werewolfapi/player/interfaces/IPlayerWW.java
+++ src/main/java/fr/ph1lou/werewolfapi/player/interfaces/IPlayerWW.java
@@ -27,25 +27,25 @@ public interface IPlayerWW {
      * Increase player max health
      * @param health the amount of health
      */
-    void addPlayerMaxHealth(int health);
+    void addPlayerMaxHealth(double health);
 
     /**
      * Decrease player max health (note : player min health is 1)
      * @param health the amount of health
      */
-    void removePlayerMaxHealth(int health);
+    void removePlayerMaxHealth(double health);
 
     /**
      * Increase player health
      * @param health the amount of health
      */
-    void addPlayerHealth(int health);
+    void addPlayerHealth(double health);
 
     /**
      * Decrease player health
      * @param health the amount of health
      */
-    void removePlayerHealth(int health);
+    void removePlayerHealth(double health);
 
     /**
      * Send String message to the player (if he was disconnected, he received the message later)
@@ -256,7 +256,14 @@ public interface IPlayerWW {
      * Get Player Max Health
      * @return max health
      */
-    int getMaxHealth();
+    double getMaxHealth();
+
+
+    /**
+     * Get Player Health
+     * @return health
+     */
+    double getHealth();
 
     /**
      * Get Player Position or if he's disconnected his last position
