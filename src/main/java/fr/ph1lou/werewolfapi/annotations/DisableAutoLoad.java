fb8c86401b7d323028cf698b9b285a453847b65d
phil <email@email.com>
2022-05-18 21:57:17 +0200

    add disable annotation

--- src/main/java/fr/ph1lou/werewolfapi/annotations/DisableAutoLoad.java
+++ src/main/java/fr/ph1lou/werewolfapi/annotations/DisableAutoLoad.java
@@ -1,5 +1,12 @@
 package fr.ph1lou.werewolfapi.annotations;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface DisableAutoLoad {
     boolean isDisable() default true;
 }
