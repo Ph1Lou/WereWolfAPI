52f1a915c18118eb09184bc48b5a58c583f79496
Hephaisto-dev <hephaistopl@gmail.com>
2023-01-11 00:33:21 +0100

    Add role base enum and event

---
+++ src/main/java/fr/ph1lou/werewolfapi/events/roles/bonesetter/BonesetterHealEvent.java
@@ -0,0 +1,23 @@
+package fr.ph1lou.werewolfapi.events.roles.bonesetter;
+
+import fr.ph1lou.werewolfapi.events.roles.SelectionEvent;
+import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
+import org.bukkit.event.HandlerList;
+import org.jetbrains.annotations.NotNull;
+
+public class BonesetterHealEvent extends SelectionEvent {
+    public BonesetterHealEvent(IPlayerWW playerWW, IPlayerWW targetWW) {
+        super(playerWW, targetWW);
+    }
+    private static final HandlerList HANDLERS_LIST = new HandlerList();
+
+    @NotNull
+    @Override
+    public HandlerList getHandlers() {
+        return HANDLERS_LIST;
+    }
+
+    public static HandlerList getHandlerList() {
+        return HANDLERS_LIST;
+    }
+}
