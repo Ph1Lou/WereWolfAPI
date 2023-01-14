b1f6fcb1a3facd1ff272675d94ebbe1da8e1a393
Hephaisto-dev <hephaistopl@gmail.com>
2023-01-14 16:57:05 +0100

    Remove useless Handler list and add bone setter choose event

--- src/main/java/fr/ph1lou/werewolfapi/events/roles/bonesetter/BonesetterChooseEvent.java
+++ src/main/java/fr/ph1lou/werewolfapi/events/roles/bonesetter/BonesetterChooseEvent.java
@@ -3,8 +3,8 @@ package fr.ph1lou.werewolfapi.events.roles.bonesetter;
 import fr.ph1lou.werewolfapi.events.roles.SelectionEvent;
 import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
 
-public class BonesetterHealEvent extends SelectionEvent {
-    public BonesetterHealEvent(IPlayerWW playerWW, IPlayerWW targetWW) {
+public class BonesetterChooseEvent extends SelectionEvent {
+    public BonesetterChooseEvent(IPlayerWW playerWW, IPlayerWW targetWW) {
         super(playerWW, targetWW);
     }
 }
