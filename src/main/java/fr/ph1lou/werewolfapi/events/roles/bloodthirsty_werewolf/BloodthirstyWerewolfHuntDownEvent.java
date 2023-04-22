2270c06d269970678ad7b17677f12f48646608a8
Combattant54 <combat54ant@gmail.com>
2023-04-22 11:40:19 +0200

    Mise en action racontable de la traque d'un joueur du loup garou sanquinnaire

--- src/main/java/fr/ph1lou/werewolfapi/events/roles/bloodthirsty_werewolf/BloodthirstyWerewolfHuntDownEvent.java
+++ src/main/java/fr/ph1lou/werewolfapi/events/roles/bloodthirsty_werewolf/BloodthirstyWerewolfHuntDownEvent.java
@@ -1,11 +1,13 @@
 package fr.ph1lou.werewolfapi.events.roles.bloodthirsty_werewolf;
 
+import fr.ph1lou.werewolfapi.annotations.TellableStoryEvent;
 import fr.ph1lou.werewolfapi.annotations.statistics.StatisticsEvent;
 import fr.ph1lou.werewolfapi.events.roles.SelectionEvent;
 import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
 import org.bukkit.event.HandlerList;
 import org.jetbrains.annotations.NotNull;
 
+@TellableStoryEvent
 @StatisticsEvent(key = "werewolf.bloodthirsty_werewolf_hunt_down_event")
 public class BloodthirstyWerewolfHuntDownEvent extends SelectionEvent {
     public static HandlerList HANDLERS_LIST = new HandlerList();
