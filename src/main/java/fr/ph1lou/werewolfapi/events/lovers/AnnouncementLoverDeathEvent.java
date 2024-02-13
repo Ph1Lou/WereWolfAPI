fb2c7fe90ac9ee8a8081c55f87d09890398c3dca
philj <phil@ou.fr>
2024-02-13 19:57:44 +0100

    - fix bugs

--- src/main/java/fr/ph1lou/werewolfapi/events/lovers/AnnouncementLoverDeathEvent.java
+++ src/main/java/fr/ph1lou/werewolfapi/events/lovers/AnnouncementLoverDeathEvent.java
@@ -1,15 +1,25 @@
 package fr.ph1lou.werewolfapi.events.lovers;
 
 import fr.ph1lou.werewolfapi.events.game.life_cycle.AnnouncementDeathEvent;
+import fr.ph1lou.werewolfapi.lovers.ILover;
 import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
 
 public class AnnouncementLoverDeathEvent extends AnnouncementDeathEvent {
+
+
     /**
      * @param playerWW     the player dead
      * @param targetPlayer the player who will show the message
      * @param format       the message
      */
-    public AnnouncementLoverDeathEvent(IPlayerWW playerWW, IPlayerWW targetPlayer, String format) {
+    private ILover lover;
+
+    public AnnouncementLoverDeathEvent(ILover lover, IPlayerWW playerWW, IPlayerWW targetPlayer, String format) {
         super(playerWW, targetPlayer, format);
+        this.lover = lover;
+    }
+
+    public ILover getLover() {
+        return lover;
     }
 }
