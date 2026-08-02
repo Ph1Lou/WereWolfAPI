643b7be5c9d2f2b717a5b3eae265b5166b69b47d
philj <phil@ou.fr>
2026-08-02 17:58:13 +0200

    - lg s10

--- src/main/java/fr/ph1lou/werewolfapi/vote/IUrnVoteManager.java
+++ src/main/java/fr/ph1lou/werewolfapi/vote/IUrnVoteManager.java
@@ -1,5 +1,13 @@
 package fr.ph1lou.werewolfapi.vote;
 
+import fr.ph1lou.werewolfapi.player.interfaces.IPlayerWW;
+
 public interface IUrnVoteManager {
     boolean isActive();
+
+    /**
+     * Mark a player so that their ballot cast during the given urn-vote session
+     * is consumed but NOT counted at dépouillement (Raven penalty).
+     */
+    void markBallotDiscarded(IPlayerWW player, int session);
 }
